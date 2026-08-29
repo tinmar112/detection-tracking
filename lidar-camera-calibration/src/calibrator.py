611c841e3b9089890ab8acb961831c559cdbc41f
Martin Zadora <martin.zadora@etu.minesparis.psl.eu>
2026-08-29 12:05:58 +0200

    reorganised overlay

--- lidar-camera-calibration/src/calibrator.py
+++ lidar-camera-calibration/src/calibrator.py
@@ -1,15 +1,27 @@
+import cv2
 import numpy as np
 
 
 class Calibrator:
 
-    def __init__(self) -> None:
+    def __init__(self, path_im: str,
+                 path_lidar: str,
+                 path_calib: str) -> None:
+
+        self._path_im = path_im
+        self._path_lidar = path_lidar
+        self._path_calib = path_calib
 
         self.matrix = np.empty((3,4))
-        
-    def load_matrices(self, path :str) -> None:
 
-        with open(file=path, mode='r') as doc:
+    def load_image(self) -> None:
+
+        self.img = cv2.imread(self._path_im)
+        self.H, self.W = self.img.shape[:2] #type: ignore
+        
+    def load_matrices(self) -> None:
+
+        with open(file=self._path_calib, mode='r') as doc:
             lines = doc.readlines()
 
         p2 = np.array(lines[2].split(':')[1].strip().split(),
@@ -27,20 +39,35 @@ class Calibrator:
         
         self.matrix = p2 @ r0_rect @ tr_velo_to_cam
 
-    def load_lidar(self, path: str) -> tuple:
+    def project_lidar(self) -> None:
 
-        self.lidar_points = np.fromfile(path, dtype=np.float32).reshape(-1, 4)
+        raw_points = np.fromfile(self._path_lidar, dtype=np.float32).reshape(-1, 4)
 
         # convert to image pixels
 
-        pts, reflectance = self.lidar_points[:, :3], self.lidar_points[:, 3]
+        pts, reflectance = raw_points[:, :3], raw_points[:, 3]
         n = pts.shape[0]
         h_pts = np.hstack([pts, np.ones((n, 1))]) # to homogeneous coordinates
         im_pts = self.matrix @ h_pts.T # (3x4) @ (4, N) -> needs (x,y,z,1) vertically
 
         u = im_pts[0] / im_pts[2] # back to cartesian
         v = im_pts[1] / im_pts[2]
-        #self.lidar_pixels = np.stack([u, v, reflectance], axis=1)
 
-        return im_pts, u, v, reflectance
+        # only keep valid points
+        depth = im_pts[2] # z in camera frame (metres)
+        valid = (depth > 0) & (u >= 0) & (u < self.W) & (v >= 0) & (v < self.H)
+        u_v, v_v, r_v = u[valid], v[valid], reflectance[valid]
+
+        self.lidar_points = im_pts
+        self.u = u_v
+        self.v = v_v
+        self.r = r_v
+
+    def load_lidar(self) -> tuple:
+
+        self.load_image()
+        self.load_matrices()
+        self.project_lidar()
+
+        return self.lidar_points, self.u, self.v, self.r
     
--- lidar-camera-calibration/src/main.py
+++ lidar-camera-calibration/src/main.py
@@ -1,15 +1,11 @@
-from calibrator import Calibrator
 from overlay import overlay
 
 if __name__ == '__main__':
 
-    calibrator = Calibrator()
-    path_calib = '../data/data_object_calib/training/calib/000055.txt'
-    calibrator.load_matrices(path=path_calib)
+    frame_id = '000150'
 
-    path_lidar = '../data/data_object_velodyne/training/velodyne/000055.bin'
-    calibrator.load_lidar(path_lidar)
-
-    path_im = '../data/data_object_image_2/training/image_2/000055.png'
+    path_calib = f'../data/data_object_calib/training/calib/{frame_id}.txt'
+    path_lidar = f'../data/data_object_velodyne/training/velodyne/{frame_id}.bin'
+    path_im = f'../data/data_object_image_2/training/image_2/{frame_id}.png'
 
     overlay(path_im, path_lidar, path_calib)
